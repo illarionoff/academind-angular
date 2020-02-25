d8bf76046cf9f76d462c0d7aeb7eaf6e78c27798
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2020-02-25 23:01:56 +0300

    Add basic structure to recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipes.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("test", "test desc", "https://via.placeholder.com/150"),
+    new Recipe("test", "test desc", "https://via.placeholder.com/150"),
+    new Recipe("test", "test desc", "https://via.placeholder.com/150")
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
